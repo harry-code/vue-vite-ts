21aa08ac4aaddc5f8b13c9aeb75625b63e971258
刘尧同学 <724869115@qq.com>
2021-05-18 13:59:49 +0800

    feat: 修改代码

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  this is app
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
-})
+    HelloWorld,
+  },
+});
 </script>
 
 <style>
